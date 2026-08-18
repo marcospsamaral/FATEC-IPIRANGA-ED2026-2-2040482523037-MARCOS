177150c8e8f43953e0295d36170700914a3f5d08
Marcos Amaral <marcos.pkinho@gmail.com>
2026-08-18 20:40:34 -0300

    Atualizar o EDI-Atividade-N1-1.c

--- EDI-2026-ATIVIDADES/EDI-Atividade-N1/EDI-Atividade-N1-1.c
+++ EDI-2026-ATIVIDADES/EDI-Atividade-N1/EDI-Atividade-N1-1.c
@@ -1,1 +1,28 @@
-void main() {}
+#define TAMANHO_TURMA 5;
+
+void main() {
+    float notas[TAMANHO_TURMA];
+    
+    exibirCabecalho();
+
+    for (int i = 0; i < TAMANHO_TURMA; i++) {
+        printf("Nota do aluno %d: ", i);
+        scanf("%f", notas[i]);
+    }
+}
+
+void exibirCabecalho() {
+    printf("=================================
+");
+    printf("SISTEMA DE NOTAS - TURMA ADS");
+    printf("=================================
+\n");
+}
+
+float calcularMedia(float vetor[], int tamanho) {
+    
+}
+
+void aplicarBonus(float *nota, float bonus) {
+    
+}
